ee1343498d6e3c7ab2a5d4a8c30d90b873bceeab
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-12-09 01:25:13 +0300

    2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld helloWorld2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("Равны ли между собой бины helloWorld ?" + (helloWorld1 == helloWorld2));
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Равны ли между собой бины catBean ?" + (cat1 == cat2));
+
     }
 }
